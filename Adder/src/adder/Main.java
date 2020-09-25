022e5fb12c7d452d540143f4ade57bf9dbf2f777
Hannah <hannahismail711@gmail.com>
2020-09-24 20:43:56 -0500

    modified adder function to subtract

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -12,6 +12,10 @@ public class Main {
     }
 
     private static int addArguments(String[] args) {
+        if (args[0] == "-")
+        {
+            return (Integer.valueOf(args[1])*-1) + (Integer.valueOf(args[2])*-1) ;
+        }
         return Integer.valueOf(args[0]) + Integer.valueOf(args[1]) + Integer.valueOf(args[2]);
     }
 }
