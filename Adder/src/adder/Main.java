37c344186fa5999f8b30972f6b1890d7923fee5f
Hannah <hannahismail711@gmail.com>
2020-09-24 21:03:31 -0500

    Added exception for invalid input type

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,9 +7,11 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
+            if(args[0].matches(".*[1-9].*") != true ||args[1].matches(".*[1-9].*") != true || args[2].matches(".*[1-9].*") != true){
+                throw new IllegalArgumentException("Input must be integers");
+            }
             System.err.println("Please provide three integers to add");
-        }
-    }
+        }}
 
     private static int addArguments(String[] args) {
         return Integer.valueOf(args[0]) + Integer.valueOf(args[1]) + Integer.valueOf(args[2]);
